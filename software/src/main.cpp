3f0ebbdccbbd345c74559fa282d01e46af6e2746
andychiem19 <andy.chiem19@gmail.com>
2025-09-04 18:22:02 -0400

    yea

---
+++ software/src/main.cpp
@@ -0,0 +1,7 @@
+#include <websocketpp/config/asio_no_tls_client.hpp>
+#include <websocketpp/client.hpp>
+#include <iostream>
+#include <chrono>
+#include <ctime>
+#include <string>
+#include <nlohmann/json.hpp>
