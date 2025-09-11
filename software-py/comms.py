4a081e31afa0048cd8cad1b48060711d7ba38828
Derek Tran <derek.tuan.tran@outlook.com>
2025-09-10 21:39:10 -0400

    9/10/25 commit, comms.py added

---
+++ software-py/comms.py
@@ -0,0 +1,29 @@
+import websockets
+import asyncio
+import json
+import serial
+
+ser = serial.Serial("COM3", 115200) # port n baud rate
+
+def pack_values(last_str, vol_str): # converts strings to integers and packs them into bytes for fpga-chan
+    last_int = int(float(last_str) * 100)
+    vol_int  = int(float(vol_str))
+    lp_bytes = last_int.to_bytes(4, 'big', signed=True)
+    vol_bytes = vol_int.to_bytes(4, 'big', signed=True)
+    return lp_bytes + vol_bytes
+
+async def main(): 
+    url = "wss://ws.okx.com:8443/ws/v5/public"
+    async with websockets.connect(url) as ws: # send subscription message (i think)
+        sub = {"op": "subscribe","args": [{"channel": "tickers", "instId": "ETH-USDT"}]}
+        await ws.send(json.dumps(sub))
+        while True:
+            msg = await ws.recv()
+            data = json.loads(msg)
+            if "data" in data:
+                t = data["data"][0]
+                frame = pack_values(t["last"], t["vol24h"])
+                ser.write(frame)
+                print([hex(b) for b in frame])
+
+asyncio.run(main())
