69f33b3296bb4748eb1b376457179b2e297f3a00
andychiem19 <andy.chiem19@gmail.com>
2025-09-10 21:57:00 -0400

    deleting C++

--- software/src/main.cpp
+++
@@ -1,7 +0,0 @@
-#include <websocketpp/config/asio_no_tls_client.hpp>
-#include <websocketpp/client.hpp>
-#include <iostream>
-#include <chrono>
-#include <ctime>
-#include <string>
-#include <nlohmann/json.hpp>
